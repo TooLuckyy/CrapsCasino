7b45d3532cd085db115c187d3c9a618c2e78162c
TooLuckyy <darreljelly@gmail.com>
2026-09-08 21:13:45 -0500

    Added a way to validate player roll eligibility

--- Backend/games/craps.py
+++ Backend/games/craps.py
@@ -7,6 +7,7 @@ class Status(Enum):
     POINT = 2
     WINNER = 3
     CRAP_OUT = 4
+    SEVEN_OUT = 5
     
 
 
@@ -18,15 +19,20 @@ class CrapsGame:
         
     #rolls 2 dice outputing a random val 1-6 while also return sum of both die
     def roll(self):
-        die1 = np.random.randint(1, 7)
-        die2 = np.random.randint(1, 7)
+        die_1 = np.random.randint(1, 7)
+        die_2 = np.random.randint(1, 7)
     
-        return die1, die2, die1 + die2
+        return die_1, die_2, die_1 + die_2
     
     #process game sequence and updates game state and rolls
     def process_roll(self):
+        #checks if player is allowed to roll and return None if not valid
+        can_roll = self.check_status()
+        if not can_roll:
+            return None
+            
         player_roll = self.roll()
-        total = player_roll[2]
+        total       = player_roll[2]
         
         #if comeout roll, come out rules are applied
         if self.status == Status.COME_OUT:
@@ -38,7 +44,7 @@ class CrapsGame:
                 
             else:
                 self.status = Status.POINT
-                self.point = player_roll[2]
+                self.point  = player_roll[2]
                 
         #if rolling for point, point rules applied
         elif self.status == Status.POINT:
@@ -46,7 +52,16 @@ class CrapsGame:
                 self.status = Status.WINNER
                 
             elif total == 7:
-                self.status = Status.CRAP_OUT
+                self.status = Status.SEVEN_OUT
+        
+        return player_roll
+    
+    #checks current game state and validates if player can roll
+    def check_status(self):
+        if self.status in [Status.WINNER, Status.CRAP_OUT, Status.SEVEN_OUT]:
+            return False
+        
+        return True
         
         
         
